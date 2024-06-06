a3dceaf15655f0848d4918ce25f5f913a3134428
Amirhossein Alian <alianmail81@gmail.com>
2024-06-06 15:10:11 +0330

    initial commit

---
+++ src/wordle.cpp
@@ -0,0 +1,11 @@
+// wordle.cpp
+// backend of WORDLE Game (guess a word)
+#include <iostream>
+using namespace std;
+
+// function main begins program execution
+int main()
+{
+	cout << "Wordle App\n";
+	return 0;
+} // end function main
