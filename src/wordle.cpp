7837761c75c75246cd374cb2fcb1bbbfac88f481
Amirhossein Alian <alianmail81@gmail.com>
2024-06-11 15:04:24 +0300

    read words from valids.txt and store them in vector

--- src/wordle.cpp
+++ src/wordle.cpp
@@ -1,11 +1,26 @@
 // wordle.cpp
 // backend of WORDLE Game (guess a word)
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+vector <string> words;
 
 // function main begins program execution
 int main()
 {
-	cout << "Wordle App\n";
+	string line = "";
+	ifstream file("valids.txt");
+	if(file.is_open()) {
+		while(getline(file, line)) {
+			words.push_back(line);
+		}
+		file.close();
+	} else {
+		cout << "[out-err]: Unable to open file ";
+	}
 	return 0;
 } // end function main
